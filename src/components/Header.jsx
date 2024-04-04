c329df50b6fa09d1a23de93086d96c57973bb4a0
itsme-ebu <itsmeebu@gmail.com>
2024-04-04 11:41:11 +0530

    error fixes 0.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { IoLogOutOutline } from "react-icons/io5";
 import { BsSun } from "react-icons/bs";
-import axios from "axios";
-import { useRouter } from "next/navigation";
+// import axios from "axios";
+// import { useRouter } from "next/navigation";
 function Header({ user }) {
-  const router = useRouter();
-  async function logout() {
-    await axios.post("/api/logout");
-    return router.push("/");
-  }
+  // const router = useRouter();
+  // async function logout() {
+  //   await axios.post("/api/logout");
+  //   return router.push("/");
+  // }
 
   return (
     <nav className={`w-full flex justify-between px-5 py-6 bg-black`}>
@@ -16,7 +16,7 @@ function Header({ user }) {
         <BsSun size={"1.3em"} />
       </div>
       <h2>EXPENSES</h2>
-      <div className=" cursor-pointer" onClick={logout}>
+      <div className=" cursor-pointer">
         <IoLogOutOutline size={"1.3em"} />
       </div>
     </nav>
